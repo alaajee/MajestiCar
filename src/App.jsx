f6764e0902b73fb44d20e1746aff874ced1493bf
jenninea <alaa.jennine@grenoble-inp.org>
2025-09-19 19:54:55 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import image1 from './assets/Photodecouv.jpeg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div className="App">
+      <div className="top-banner">
+        <div className="slogan">✨ Bienvenue sur notre site ! Découvrez nos services exceptionnels ✨</div>
+      </div>
+      <header className="App-header">
+        <div className="menu-container">
+          <div className="menu-item">Home</div>
+          <div className="menu-item">A propos</div>
+          <div className="menu-item">Services</div>
+          <div className="menu-item">Contact</div>
+        </div>
+      </header>
+      <div className="Photo">
+        <img src={image1} alt="A beautiful scenery" />
+        <div className="image-slogan">
+          Votre voiture, notre soin, partout avec MugiWash.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
