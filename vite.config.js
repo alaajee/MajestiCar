713c4986889bdaf785a477bb4fee690f651c5a82
jenninea <alaa.jennine@grenoble-inp.org>
2025-09-19 21:15:05 +0200

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/MugiWash/' : '/src/main.jsx',
+  base: '/MugiWash/', // Important: doit correspondre au nom de votre repo
+  build: {
+    outDir: 'dist',
+  },
 })
