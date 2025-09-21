186dd4bf6739ecff5edd0b75330043cf1e1c6ce2
jenninea <alaa.jennine@grenoble-inp.org>
2025-09-21 17:41:31 +0200

    hh

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom' // HashRouter au lieu de BrowserRouter
 import App from './App.jsx'
-import Reserver from './components/Reserver.jsx' // Créez ce fichier
+import Reserver from './components/Reserver.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* Changement ici */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/reserver" element={<Reserver />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
